02c643f4697bd095f35d21ec9137cf30ffd7c4fe
Amit Philips <amitphilips@live.com>
2017-12-01 19:36:09 +0530

    Enabled migrations using nuget packages and create a migration

--- AmpsBlog.API/BlogDbContext.cs
+++ AmpsBlog.API/BlogDbContext.cs
@@ -5,12 +5,17 @@ namespace AmpsBlog.API
 {
     public class BlogDbContext : DbContext
     {
-        public BlogDbContext(string connectionString)
+        public BlogDbContext(DbContextOptions<BlogDbContext> options) : base(options)
         {
             
         }
 
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
     }
 }
